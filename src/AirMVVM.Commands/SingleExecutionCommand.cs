f74219a69c430a66ccd940429acc5af330a388c3
Eugene <m_ec@mail.ru>
2018-08-29 12:31:42 +0300

    Add skeleton for SingleExecutionCommand

---
+++ src/AirMVVM.Commands/SingleExecutionCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AirMVVM.Commands
+{
+    public class SingleExecutionCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        private bool _commandStarted = false;
+
+        public SingleExecutionCommand(Action execute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = execute;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        private void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return !_commandStarted;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_commandStarted)
+            {
+                return;
+            }
+            _commandStarted = true;
+            RaiseCanExecuteChanged();
+            _execute();
+        }
+    }
+}
